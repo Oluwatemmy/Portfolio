00a428e2b819ed7d912f4821c4b89863b06d2e74
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-01-17 16:28:09 +0100

    updating images to php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include_once("portfolio/index.php"); ?>
+<?php include_once("portfolio/index.html"); ?>
 <style>
     <?php include 'portfolio/style.css'; ?>
+    <?php include 'portfolio/AOTEM.jpg'; ?>
+    <?php include 'portfolio/bg.png'; ?>
+    <?php include 'portfolio/mylogo-removebg-preview.png'; ?>
 </style>
