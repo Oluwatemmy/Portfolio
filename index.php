919682250bd494c01047ea79df810a922cffd17f
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-01-17 17:21:52 +0100

    Revert "Update index.php"
    This reverts commit 15d4a1edf28324ab3cd0b243b64b56d5fe3a38e5.

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php include_once("portfolio/index.php"); ?>
 <style>
-    <?php include 'portfolio/image'; ?>
     <?php include 'portfolio/style.css'; ?>
+</style>
+<style>
+    <?php include 'portfolio/image'; ?>
 </style>
