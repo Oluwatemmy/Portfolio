15d4a1edf28324ab3cd0b243b64b56d5fe3a38e5
Ajayi Oluwaseyi <oluwaseyitemitope456@gmail.com>
2023-01-17 16:07:32 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php include_once("portfolio/index.php"); ?>
 <style>
-    <?php include 'portfolio/style.css'; ?>
-</style>
-<style>
     <?php include 'portfolio/image'; ?>
+    <?php include 'portfolio/style.css'; ?>
 </style>
